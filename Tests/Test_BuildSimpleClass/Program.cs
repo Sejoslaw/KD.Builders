3ca93096ff0cee3fc1f7e2f035107f4b44f6b9f7
Krzysztof Dobrzyński <k.dobrzynski94@gmail.com>
2018-06-28 10:19:00 +0200

    Added comments and sample object of new type.

--- Tests/Test_BuildSimpleClass/Program.cs
+++ Tests/Test_BuildSimpleClass/Program.cs
@@ -7,12 +7,20 @@ namespace Test_BuildSimpleClass
     {
         static void Main(string[] args)
         {
+            // Get Class Builder
             IClassBuilder builder = BuilderFactory.Resolve<IClassBuilder>();
 
+            // Create new Type
             Type newType = builder.CreateType("NewAwesomeAssembly", "KD.Builders.Dynamic", "MyNewAwesomeType",
                 new string[] { "Id", "Name", "Year" },
                 new Type[] { typeof(Guid), typeof(string), typeof(int) },
                 null);
+
+            // Create sample object and set it's values.
+            dynamic inst = Activator.CreateInstance(newType);
+            inst.Id = Guid.NewGuid();
+            inst.Name = "Krzysztof";
+            inst.Year = 1234567890;
 
             Console.WriteLine($"New type created: { newType }");
         }
